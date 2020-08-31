967f89bac382146c0e1cd5e575db83cfb66ce2f3
CourtneyJa <courtney.jackson08234@gmail.com>
2020-08-30 21:57:51 -0400

    basic styling added, still running in to errors

--- game-of-life/src/Components/Row.js
+++ game-of-life/src/Components/Row.js
@@ -3,20 +3,20 @@ import Cell from "./Cell";
 import "./game.css";
 
 const Row = (props) => {
-  const height = ((1 / props.size) * 100).toString();
+  const height = (1 / props.size * 100).toString()
   return (
     <div className="row">
-      {props.cells.map((cell, idx) => {
+      {props.cells.map((cell, idx) => (
         <Cell
           key={idx}
           value={cell}
           yAxis={props.yAxis}
           xAxis={props.xAxis}
           running={props.running}
-          toggle={props.toggleCells}
+          toggle={props.toggle}
           size={props.size}
-        />;
-      })}
+        />
+      ))}
     </div>
   );
 };
